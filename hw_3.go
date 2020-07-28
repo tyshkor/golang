0dbfd7b6eaf4f058068e61883fdf3e370c33ed53
tyshkor <67144915+tyshkor@users.noreply.github.com>
2020-07-28 10:04:44 +0300

    Create hw_3.go

---
+++ hw_3.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type worker struct {
+	pers *human
+	exp  int
+}
+
+type human struct {
+	name string
+	age  int
+}
+
+type hr struct {
+	wor     *worker
+	country string
+	field   string
+}
+
+type coder struct {
+	wor           worker
+	primaryTech   string
+	secondaryTech string
+}
+
+func (receiver *human) HappyBirthday() {
+	fmt.Printf("Happy Birthday yo you %v!\n", (*receiver).name)
+}
+
+func (receiver *human) promoted() string {
+
+	res := "You are promoted!"
+	return res
+}
+
+func (receiver *human) yourAge5YearsAgo() int {
+
+	res := (*receiver).age - 5
+	return res
+}
+
+func main() {
+
+	q := human{"James", 29}
+
+	q.HappyBirthday()
+
+	fmt.Println(q.promoted())
+
+	fmt.Printf("Your age 5 yers ago was %d", q.yourAge5YearsAgo())
+  
+	//panic("some problem")
+	var a [2]int
+	fmt.Print(a[2])
+}
